d931126b7b619ae63b8fbf5e4bd3941a542e1d38
dfgiordano <federico.giordano90@gmail.com>
2021-05-03 16:46:33 +0200

    funzione

---
+++ js/script.js
@@ -0,0 +1,43 @@
+/* 1 - Palindroma
+Chiedere all'utente di inserire una parola */
+var parolaUtente = prompt("Inserisci una parola");
+console.log(parolaUtente);
+var parolaRovesciata;
+/* Creare una funzione per capire se la parola inserita è palindroma */
+if (parolaUtente == parolaRovesciata) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
+function parolaAlContrario (parolaUtente,parolaRovesciata) {
+var parolaAlContrario;
+for (i = 0; i < parolaUtente.length -1; i--) {
+}
+console.log(parolaUtente[i]);
+return 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 2 - Pari e Dispari 
+L'utente sceglie pari o dispari e inserisce un numero da 1 a 5. */
+
+
+/* Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
