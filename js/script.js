658f40923e701e1a608b6ab8fa7d75c4dcbdfdb8
dfgiordano <federico.giordano90@gmail.com>
2021-05-03 18:54:40 +0200

    esercizio parola terminato

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-// /* 1 - Palindroma
-// Chiedere all'utente di inserire una parola */
-// var parolaUtente = prompt("Inserisci una parola");
-// console.log(parolaUtente);
-// var parolaRovesciata;
-// /* Creare una funzione per capire se la parola inserita è palindroma */
+/* 1 - Palindroma
+Chiedere all'utente di inserire una parola */
+var parolaUtente = prompt("Inserisci una parola");
+console.log(parolaUtente);
+var parolaRovesciata;
+/* Creare una funzione per capire se la parola inserita è palindroma */
 
-// function parolaAlContrario (parola) {
-//     var risultato = "";
-//     var len = parola.length;
-//     /*
-//     parola = "topo"
-//     i parte da 3
-//     i>=0
-//     */
-//     for(var i = len-1; i >= 0; i--) {
-//         risultato += parola[i];
-//     }
-//     return risultato;
-// }
+function parolaAlContrario (parola) {
+    var risultato = "";
+    var len = parola.length;
+    /*
+    parola = "topo"
+    i parte da 3
+    i>=0
+    */
+    for(var i = len-1; i >= 0; i--) {
+        risultato += parola[i];
+    }
+    return risultato;
+}
 
-// parolaRovesciata = parolaAlContrario(parolaUtente);
+parolaRovesciata = parolaAlContrario(parolaUtente);
 
-// if (parolaUtente == parolaRovesciata) {
-//     console.log("La parola è palindroma");
-// } else {
-//     console.log("La parola non è palindroma");
-// }
+if (parolaUtente == parolaRovesciata) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
 
 /* 2 - Pari e Dispari 
 L'utente sceglie pari o dispari e inserisce un numero da 1 a 5. */
